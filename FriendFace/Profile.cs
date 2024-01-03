843e3c732385ada73f62a61710e1a7ead9f63856
Biner Habib <Binerhabib@gmail.com>
2024-01-03 13:32:29 +0100

    Add project files.

---
+++ FriendFace/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendFace
+{
+    internal class Profile
+    {
+        private string Name {  get; set; }
+        private string Nickname { get; set; }
+        private int Age { get; set; }
+        private string Education { get; set; }
+        private string Work {  get; set; }
+        private string PlaceOfLiving { get; set; }
+
+        public Profile(string name, string nickname, int age, string education, string work, string placeofliving) { }
+    }
+}
