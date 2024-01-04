fd98ecf272e353e2b6827e936d2b69bd93395391
Biner Habib <Binerhabib@gmail.com>
2024-01-04 15:01:38 +0100

    1

--- FriendFace/Program.cs
+++ FriendFace/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using FriendFace;
+
+List<Profile> list = new List<Profile>()
+{
+    new Profile("Terje", "Terje10", 30, "Larvik vgs", "Koding", "Larvik"),
+    new Profile("Ola Normann", "Ola22", 25, "Trygg vgs", "Tømrer", "Oslo"),
+    new Profile("Ahmed", "Ahmed64", 28, "Stovner vgs", "Advokat", "Oslo")
+};
+
+
--- FriendFace/View.cs
+++ FriendFace/View.cs
@@ -8,5 +8,53 @@ namespace FriendFace
 {
     internal class View
     {
+        public List<Profile> Profiles { get; set; }
+
+        public View(List<Profile> list) 
+        {
+            Profiles = list;
+        }
+
+        public void ShowFriendMenu()
+        {
+            for (int i = 0; i < Profiles.Count; i++)
+            {
+                Console.WriteLine($"{i}.{Profiles[i].GetName()} {Profiles[i].GetNickname()}");
+            }
+        }
+
+        public void ShowFriends()
+        {
+            for (int i = 0; i < Profiles.Count; i++)
+            {
+                Console.WriteLine($"{Profiles[i].GetName()} {Profiles[i].GetNickname()} {Profiles[i].GetAge()} {Profiles[i].GetEducation()} {Profiles[i].GetWork()} {Profiles[i].GetPlaceOfLiving()}");
+            }
+        }
+
+        public void RemoveFriend()
+        {
+
+        }
+
+        public void menu()
+        {
+            var profile = new Profile();
+
+            profile.GenerateProfile();
+
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    ShowFriendMenu(); break;
+                case "2":
+                     break;
+                case "3":
+                    RemoveFriend(); break;
+                case "4":
+                    ShowFriends(); break;
+                default: break;
+            }
+        }
     }
 }
